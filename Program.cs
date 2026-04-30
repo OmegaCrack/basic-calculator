6e4e0b2ec9538aacd203ef3ec86562f145a9d0a5
charles <charlesomukaya3@gmail.com>
2026-04-30 13:56:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+class Calculator
+{
+    static void Main()
+    {
+        Console.Write("Enter first number  : ");
+        double a = double.Parse(Console.ReadLine() ?? "0");
+ 
+        Console.Write("Enter operator (+, -, *, /): ");
+        string op = Console.ReadLine() ?? "+";
+ 
+        Console.Write("Enter second number : ");
+        double b = double.Parse(Console.ReadLine() ?? "0");
+ 
+        double result = op == "+" ? a + b
+                      : op == "-" ? a - b
+                      : op == "*" ? a * b
+                                  : a / b;
+ 
+        Console.WriteLine($"{a} {op} {b} = {result}");
+    }
+}

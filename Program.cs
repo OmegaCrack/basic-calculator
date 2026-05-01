2f058365ddf62ebafbc219a28e7e666b4ca19a9d
charles <charlesomukaya3@gmail.com>
2026-05-01 20:59:29 +0300

    final

--- Program.cs
+++ Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
- 
-class Calculator
+
+namespace Conditional
 {
-    static void Main()
-    {
-        Console.Write("Enter first number  : ");
-        double a = double.Parse(Console.ReadLine() ?? "0");
- 
-        Console.Write("Enter operator (+, -, *, /): ");
-        string op = Console.ReadLine() ?? "+";
- 
-        Console.Write("Enter second number : ");
-        double b = double.Parse(Console.ReadLine() ?? "0");
- 
-        double result = op == "+" ? a + b
-                      : op == "-" ? a - b
-                      : op == "*" ? a * b
-                                  : a / b;
- 
-        Console.WriteLine($"{a} {op} {b} = {result}");
+    class IfStatement {
+        public static void Main(string[] args)
+        {
+            
+            int outter = 6;
+            int inner = 5;
+
+            switch (outter) {
+                case 1:
+                    Console.WriteLine("Outter case 1");
+                    break;
+                case 2: 
+                    Console.WriteLine("Outter case 2");
+                    switch (inner) {
+                        case 1:
+                            Console.WriteLine("Inner case 1");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Inner case 2");
+                            break;
+                        case 3:
+                            Console.WriteLine("Inner case 3");
+                            break;
+                        default:
+                            Console.WriteLine("Default Inner run");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Default Outter run");
+                    break;
+            }
+
+            Console.ReadKey();
+            
+        }
     }
 }
--- obj/Debug/net10.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net10.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbc47dff2c7d0937586be5bdb8dc82a3008aec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
